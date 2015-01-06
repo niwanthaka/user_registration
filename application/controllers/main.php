b91f816fb86968a329b2802ba15705ef657d6fc0
Niwanthaka Maddumarala <niwanthaka@3cs.lk>
2015-01-06 21:52:32 +0530

    Created main view

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,8 +5,7 @@ class Main extends CI_Controller {
 
 	public function index()
 	{
-		//$this->load->view('welcome_message');
-		echo "dd";
+		$this->load->view('main');
 	}
 }
 
---
+++ application/views/main.php
@@ -0,0 +1,3 @@
+<h1>Welcome to user registration form</h1>
+<p>If you already has an account please log in by entering your username and password above.
+If not please create an account by clicking on Register.</p>
