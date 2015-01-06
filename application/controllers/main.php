df1863a5c8b32c1bd3c8302dd328e426aabc841f
Niwanthaka Maddumarala <niwanthaka@3cs.lk>
2015-01-06 21:22:28 +0530

    Created main controller and change routes file to load main controller

---
+++ application/controllers/main.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+
+
+	public function index()
+	{
+		//$this->load->view('welcome_message');
+		echo "dd";
+	}
+}
+
+ 
