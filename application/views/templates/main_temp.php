fd39f50a51c51586d13cf8268f4681685ad2f7e7
Niwanthaka Maddumarala <niwanthaka@3cs.lk>
2015-01-06 21:56:33 +0530

    Created main template

---
+++ application/views/templates/main_temp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>User Registration Using CI</title>
+</head>
+<body>
+ 
+
+    <div align="center">
+ 
+ 
+      <hr>
+        <!--Footer Starts-->
+      <footer>
+        <p>&copy; Copyright 2015</p>
+      </footer>
+        <!--Footer End-->
+    </div> 
+</body>
+</html>
